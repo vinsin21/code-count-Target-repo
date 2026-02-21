09bcc2b57c5a02cf9efa1f899c6c95be288946e0
vinsin21 <vineet.singh.jrj@gmail.com>
2026-02-21 14:44:08 +0530

    to check if code-counter working properly by addint a new line

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+console.log('Hello World!')
+// this is a comment
+
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${process.env.PORT}`)
 })
